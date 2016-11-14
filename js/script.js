6bd0d6d602c34ac1076c1f8ae51043278087ee54
Kacper Grzeszczyk <kiper@desmart.com>
2016-11-14 19:14:55 +0100

    Improvement(js): fix code issues

--- js/script.js
+++ js/script.js
@@ -1,64 +1,74 @@
 $(function(){
-
-var $carouselList = $('#carousel ul');
-$carouselList.css({marginLeft: -400});
-var i = 0;
-var photos = ['#one', '#two', '#three', '#four', '#five'];
-$(photos[i]).removeClass('fa-circle-thin').addClass('fa-circle');
-
-
-function checkCounter(){
-	if (i == 5) {
-		i = 0
-	} else if (i == -1) {
-		i = 4;
-	}
-};	
-
-
-
-function changeDots(){
+	var $carouselList = $('#carousel ul');
+	$carouselList.css({marginLeft: -400});
+	var i = 0;
 	var photos = ['#one', '#two', '#three', '#four', '#five'];
-	$(photos[i - 1]).removeClass('fa-circle').addClass('fa-circle-thin');
-	$(photos[i + 1]).removeClass('fa-circle').addClass('fa-circle-thin');
+
+	photos.forEach(function(photo, index) {
+		$(photo).click(function() {
+			console.log(index);
+		});
+	});
+
 	$(photos[i]).removeClass('fa-circle-thin').addClass('fa-circle');
-	checkCounter();
-};
-	
 
-function changeSlideLeft(){
-	$carouselList.animate({'marginLeft': -800}, 500, moveFirstSlide);
-};
+	function checkCounter(){
+		if (i === 5) {
+			i = 0
+		} else if (i === -1) {
+			i = 4;
+		}
+	};
 
-function changeSlideRight(){
-	$carouselList.animate({'marginLeft': +0}, 500, moveLastSlide);
-};
+	function changeDots(){
+		checkCounter();
+		photos.forEach(function(photo) {
+			$(photo).removeClass('fa-circle').addClass('fa-circle-thin');
+		});
+		$(photos[i]).removeClass('fa-circle-thin').addClass('fa-circle');
+	};
 
-setInterval(changeSlideLeft, 3000);
+	function changeSlideLeft(){
+		$carouselList.animate({'marginLeft': -800}, 500, moveFirstSlide);
+	};
 
-function moveLastSlide(){
-	var $firstItem = $carouselList.find('li:first');
-	var $lastItem = $carouselList.find('li:last');
-	$firstItem.before($lastItem);
-	$carouselList.css({marginLeft: -400});
- 	i--;
-	changeDots();
+	function changeSlideRight(){
+		$carouselList.animate({'marginLeft': +0}, 500, moveLastSlide);
+	};
 
+	var interval = setInterval(changeSlideLeft, 3000);
 
-};
+	function moveLastSlide(){
+		var $firstItem = $carouselList.find('li:first');
+		var $lastItem = $carouselList.find('li:last');
+		$firstItem.before($lastItem);
+		$carouselList.css({marginLeft: -400});
+	 	i--;
+		changeDots();
+	};
 
-function moveFirstSlide(){
-	var $firstItem = $carouselList.find('li:first');
-	var $lastItem = $carouselList.find('li:last');
-	$lastItem.after($firstItem);
-	$carouselList.css({marginLeft: -400});
-	i++
-	changeDots();
-};
+	function moveFirstSlide(){
+		var $firstItem = $carouselList.find('li:first');
+		var $lastItem = $carouselList.find('li:last');
+		$lastItem.after($firstItem);
+		$carouselList.css({marginLeft: -400});
+		i++
+		changeDots();
+	};
 
-$('#right').click(changeSlideRight);
-$('#left').click(changeSlideLeft);
+	function resetInterval() {
+		clearInterval(interval);
+		interval = setInterval(changeSlideLeft, 3000);
+	}
 
+	$('#right').click(function() {
+		changeSlideLeft();
+		resetInterval();
+	});
+	$('#left').click(function() {
+		changeSlideRight();
+		resetInterval();
+	});
 });
 
 
